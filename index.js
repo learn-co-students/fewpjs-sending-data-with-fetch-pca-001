60f50470cbce808ae4f46f63a100abf5580a4c29
Aaron Wilson <adub65@gmail.com>
2020-07-08 16:20:11 -0400

    sending and receiving data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(name, email) {
+  let formData = {
+    name: name,
+    email: email
+  }
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(resp => { return resp.json() })
+    .then(json => {
+      const newId = document.createTextNode(json['id'])
+      document.body.appendChild(newId)
+      console.log(json)
+    })
+    .catch(errors => {
+      const error = document.createTextNode(errors)
+      document.body.appendChild(error)
+      console.log(errors)
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  submitData("Aaron","aw@email.com")
+})
